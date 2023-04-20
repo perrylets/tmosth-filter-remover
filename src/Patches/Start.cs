f7f63e103a8114ef7c5f1b3226a0c159c0820257
Perry <62119804+perrylets@users.noreply.github.com>
2023-04-19 23:33:40 -0300

    Patch the 'Start' method instead of 'Focus' and do better logs

--- src/Patches/Start.cs
+++ src/Patches/Start.cs
@@ -6,19 +6,19 @@ using HarmonyLib;
 namespace Perry.FilterRemover.Patches
 {
 
-  [HarmonyPatch(typeof(NameEntryScreen), nameof(NameEntryScreen.Focus))]
-  class Focus
+  [HarmonyPatch(typeof(NameEntryScreen), START)]
+  class Start
   {
 
     const string EXISTING_CHARACTERS = "existingCharacters";
+    const string START = "Start";
 
     static void Postfix(NameEntryScreen __instance)
     {
 
-      MelonLogger.Msg(__instance);
       FieldInfo existingCharacters = typeof(NameEntryScreen).GetField(EXISTING_CHARACTERS, BindingFlags.NonPublic | BindingFlags.Instance);
       existingCharacters.SetValue(__instance, Array.Empty<String>());
-      MelonLogger.Msg(string.Join('\n', existingCharacters.GetValue(__instance) as string[]));
+      MelonLogger.Msg($"Existing characters: \"{string.Join('\n', existingCharacters.GetValue(__instance) as string[])}\" ");
 
     }
 
